774ba33adb928721f98d188457d7a902c4a0d412
Lucas <lucasrian.dev@gmail.com>
2026-06-17 13:22:01 -0300

    estudando rotas

--- NodeCodeDex/app.js
+++ NodeCodeDex/app.js
@@ -1,16 +1,20 @@
 const http = require("http");
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { "content-type": "text/plain; charset=utf-8" });
-  response.write("Welcome to Neon Noodles!");
-  response.write("LATE NITE MENU");
-  response.write("RAMEN 1. Quantum Truffle Ramen ");
-  response.write(
-    "EXTRA TOPPINGS 1. Hacktivist Pork 2. Cybernetic Egg 3. Glowing Scallions ",
-  );
-  response.end();
+const server = http.createServer((req, res) => {
+  let statusCode = 200;
+  let contentType = "text/html; charset=utf-8";
+  let content = "";
+
+  if (req.url === "/") {
+    content = "<h1>Home</h1>";
+  } else if (req.url === "/a") {
+    content = "<h1>Sala de aula</h1>";
+  }
+
+  res.writeHead(statusCode, { "content-type": contentType });
+  res.end(content);
 });
 
 server.listen(3000, () => {
-  console.log("Servidor funcionando!");
+  console.log("Servidor funcionando");
 });
