486be92363fc20dea528106acdf3f9c5d99be03f
Lucas <lucasrian.dev@gmail.com>
2026-06-17 13:02:37 -0300

    treinando response

--- NodeCodeDex/app.js
+++ NodeCodeDex/app.js
@@ -1,10 +1,16 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.end("❌❌❌");
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "content-type": "text/plain; charset=utf-8" });
+  response.write("Welcome to Neon Noodles!");
+  response.write("LATE NITE MENU");
+  response.write("RAMEN 1. Quantum Truffle Ramen ");
+  response.write(
+    "EXTRA TOPPINGS 1. Hacktivist Pork 2. Cybernetic Egg 3. Glowing Scallions ",
+  );
+  response.end();
 });
 
 server.listen(3000, () => {
-  console.log("Servidor funcionando");
+  console.log("Servidor funcionando!");
 });
