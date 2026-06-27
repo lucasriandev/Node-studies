d5d962c1aa21eec4ffef9f927345830168321f86
Lucas <lucasrian.dev@gmail.com>
2026-06-27 12:45:30 -0300

    treinando server

--- NodeCodeDex/app.js
+++ NodeCodeDex/app.js
@@ -1,6 +1,27 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  let codeStatus = 200;
+  let contentType = "text/html; charset=utf-8";
+  let content = "";
+
+  if (req.url === "/") {
+    content = "<h1>Pagina inicial</h1>";
+  } else if (req.url === "/contato") {
+    content = "<h1>Contato!</h1>";
+  }
+
+  res.writeHead(codeStatus, { "Content-Type": contentType });
+  res.end(content);
+});
+
+server.listen(3000, () => {
+  console.log("Server ativo!");
+});
+
+/*const http = require("http");
+
+const server = http.createServer((req, res) => {
   let statusCode = 200;
   let contentType = "text/html; charset=utf-8";
   let content = "";
@@ -13,4 +34,4 @@ const server = http.createServer((req, res) => {
   res.end(content);
 });
 
-server.listen(3000, () => [console.log("Servidor ligado!")]);
+server.listen(3000, () => [console.log("Servidor ligado!")]);*/
