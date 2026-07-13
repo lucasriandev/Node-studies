8cbafb78735ffa9b1aaa73442c49bcb7ca875fcb
Lucas <lucasrian.dev@gmail.com>
2026-07-13 12:39:29 -0300

    server basico

--- NodeCodeDex/app.js
+++ NodeCodeDex/app.js
@@ -1,22 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  let statusCode = 200;
-  let contentType = "text/html; charset=utf-8";
-  let content = "";
-
   if (req.url === "/") {
-    content = "<h1>Pagina inicial!</h1>";
-  } else if (req.url === "/messi") {
-    content = "<h1>Melhor do mundo!</h1>";
+    res.write("Hello word");
+    res.end();
   }
-
-  res.writeHead(statusCode, { "Content-Type": contentType });
-  res.end(content);
 });
 
 server.listen(3000, () => {
-  console.log("Servidor ativo!");
+  console.log("Servidor ativo");
 });
 
 /*const http = require("http");
