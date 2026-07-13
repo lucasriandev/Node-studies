35f9bd5c4e2e3d48cae47528c2932cfdf2ea6c2f
Lucas <lucasrian.dev@gmail.com>
2026-07-13 13:35:25 -0300

    renderizando imagem

--- NodeCodeDex/app.js
+++ NodeCodeDex/app.js
@@ -1,37 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    console.log(req.url);
-    console.log(req.method);
+  console.log(req.url);
+  console.log(req.method);
 
-    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-    res.end("hello word");
-  } else if (req.url === "/teste") {
+  if (req.url === "/pikachu") {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.end(`<h1>Teste de clique</h1>
-      <button>Clique</button>
-      `);
+    res.end(
+      `<img src="https://tse1.mm.bing.net/th/id/OIP.PvHb7bZbrm1rCz_0DSVdVAHaEK?r=0&pid=Api&P=0&h=180">`,
+    );
   }
 });
 
 server.listen(3000, () => {
-  console.log("Servidor ativo");
+  console.log("Servidor ativo!");
 });
-
-/*const http = require("http");
-
-const server = http.createServer((req, res) => {
-  let statusCode = 200;
-  let contentType = "text/html; charset=utf-8";
-  let content = "";
-
-  if (req.url === "/") {
-    content = "<h1>Pagina inicial";
-  }
-
-  res.writeHead(statusCode, { "Content-Type": contentType });
-  res.end(content);
-});
-
-server.listen(3000, () => [console.log("Servidor ligado!")]);*/
