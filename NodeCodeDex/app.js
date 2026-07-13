949c976023d30f1814a40a6b7a0f86c672ee8691
Lucas <lucasrian.dev@gmail.com>
2026-07-13 17:19:25 -0300

    get finalizado

--- NodeCodeDex/app.js
+++ NodeCodeDex/app.js
@@ -1,10 +1,22 @@
+//GET
+
 const http = require("http");
+let like = 30;
+let compartilhamento = 2;
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
   console.log(req.method);
 
-  if (req.url === "/pikachu") {
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    like++;
+    compartilhamento++;
+    let soma = like + compartilhamento;
+    console.log(`likes ${like} e ${compartilhamento}`);
+    res.write(`${soma}`);
+    res.end("Testando get");
+  } else if (req.url === "/pikachu") {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     res.end(
       `<img src="https://tse1.mm.bing.net/th/id/OIP.PvHb7bZbrm1rCz_0DSVdVAHaEK?r=0&pid=Api&P=0&h=180">`,
