38011b8e8d5aa1edf9db4bfa4e269e526d85fb4e
Lucas <lucasrian.dev@gmail.com>
2026-07-14 11:38:56 -0300

    PUT

--- NodeCodeDex/app3.js
+++ NodeCodeDex/app3.js
@@ -2,7 +2,28 @@
 
 const http = require("http");
 
-const server = http.createServer((req, res) => {});
+let username = "lucas";
+
+const server = http.createServer((req, res) => {
+  if (req.method === "PUT") {
+    let newUsername = "";
+
+    req.on("data", (chunk) => {
+      newUsername += chunk;
+    });
+
+    req.on("end", () => {
+      console.log(`Nome de inicio ${username}`);
+      username = newUsername;
+      console.log(`Mudou para ${username}`);
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Volte ao terminal");
+  }
+});
 
 server.listen(3000, () => {
   console.log("Servidor ligado!");
