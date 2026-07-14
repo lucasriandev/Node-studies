aefc1ce417f9d6b55f261b9082a1b058ef7def67
Lucas <lucasrian.dev@gmail.com>
2026-07-14 16:11:35 -0300

    server completo

--- NodeCodeDex/teste.js
+++ NodeCodeDex/teste.js
@@ -1,26 +1,61 @@
-//curl.exe -X PUT http://localhost:3000 -d ""
-
 const http = require("http");
 
+let mensagem = "";
+
 const server = http.createServer((req, res) => {
-  console.log(req.method);
-  if (req.method === "POST") {
-    let mensagem = "";
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(`MENSAGEM GRAVADA ${mensagem}`);
+  } else if (req.method === "POST") {
+    let mensagemRecebida = "";
 
     req.on("data", (chunk) => {
-      mensagem += chunk;
+      mensagemRecebida += chunk;
     });
 
     req.on("end", () => {
+      mensagem = mensagemRecebida;
+
+      console.log(`Esta mensagem foi gravada: ${mensagem}`);
+
       res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(`Mensagem gravada ${mensagem}`);
+      res.end(`MENSAGEM GRAVADA: ${mensagem}`);
     });
+  } else if (req.method === "PUT") {
+    let novaMensagem = "";
+
+    req.on("data", (chunk) => {
+      novaMensagem += chunk;
+    });
+
+    req.on("end", () => {
+      mensagem = novaMensagem;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`${mensagem}`);
+    });
+  } else if (req.method === "PATCH") {
+    let mensagemAlterada = "";
+
+    req.on("data", (chunk) => {
+      mensagemAlterada += chunk;
+    });
+
+    req.on("end", () => {
+      mensagem += mensagemAlterada;
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(`Mensagem alterada ${mensagem}`);
+    });
+  } else if (req.method === "DELETE") {
+    console.log(`Mensagem antiga ${mensagem}`);
+    mensagem = "";
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Mensagem apagada!");
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Errooo");
+    res.end("Volte ao terminal");
   }
 });
 
 server.listen(3000, () => {
-  console.log("Servidor funcionando!");
+  console.log("servidor ativo");
 });
