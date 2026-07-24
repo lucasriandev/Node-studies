923512510b309cd7bcf8a6f3531dc3cd79a465be
Lucas <lucasrian.dev@gmail.com>
2026-07-24 17:57:32 -0300

    node com json

--- NodeExpress/teste.js
+++ NodeExpress/teste.js
@@ -1,43 +1,38 @@
 const express = require("express");
 const app = express();
 
-app.use(express.text());
+app.use(express.json());
 
-let mensagem = ["Lucas"];
+let mensagem = [];
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  res.end(`${mensagem}`);
+  res.json({ total: mensagem.length, dados: mensagem });
 });
 
 app.post("/", (req, res) => {
-  mensagem.push(req.body);
-  console.log(`Mensagem nova gravada: ${mensagem}`);
-  res.send(`Mensagem enviada do post: ${mensagem}`);
+  const novaMensagem = req.body;
+
+  mensagem.push(novaMensagem);
+
+  res.status(201).json({ status: "sucesso", mensagemCriada: novaMensagem });
 });
 
-app.put("/", (req, res) => {
+app.put("/:id", (req, res) => {
   mensagem = req.body;
-  console.log(`Mensagem do put: ${mensagem}`);
-  res.setHeader("Content-Type", "text/plain");
-  res.end(`Mensagem gravada do put: ${mensagem}`);
-});
-
-app.patch("/", (req, res) => {
-  mensagem += req.body;
-  console.log(`Mensagem gravada do patch: ${mensagem}`);
-  res.end(`Mensagem gravada do patch: ${mensagem}`);
+  res.status(201).json({ status: "sucesso", mensagemAlterada: novaMsg });
 });
 
 app.delete("/", (req, res) => {
-  mensagem = ["Lucas"];
-  res.end(`Post e alterações apagadas`);
+  mensagem = [];
+  res
+    .json(201)
+    .json({ status: "sucesso", detalhe: "Todas as mensagens foram apagadas!" });
 });
 
 app.use((req, res) => {
-  res.status(404).send("Volte ao terminal");
+  res.status(404).json({ erro: "Rota não encontrada" });
 });
 
 app.listen(3000, () => {
-  console.log("Servidor ligado!");
+  console.log("Servidor rodando na porta 3000!");
 });
