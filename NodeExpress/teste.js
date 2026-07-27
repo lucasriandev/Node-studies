217d509a3c6f455a485c07dc65d24b579bfdae2e
Lucas <lucasrian.dev@gmail.com>
2026-07-27 12:09:23 -0300

    PUT

--- NodeExpress/teste.js
+++ NodeExpress/teste.js
@@ -15,6 +15,18 @@ app.post("/", (req, res) => {
   res.status(201).json({ status: "Sucesso", dados: mensagem });
 });
 
+app.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (!mensagem.id) {
+    res.status(404).json({ erro: "Mensagem nao encontrada!" });
+  }
+
+  mensagem[id] = req.body;
+  //"Abra a gaveta de número id e coloque lá dentro o novo objeto que veio no req.body (substituindo o que estava lá antes)."
+  res.json({ status: "Sucesso", dados: mensagem[id] });
+});
+
 app.listen(3000, () => {
   console.log("Servidor ativo!");
 });
