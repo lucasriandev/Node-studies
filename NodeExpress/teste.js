ef5530d67008c2cc9b68f5b6bcdfeaef6746d263
Lucas <lucasrian.dev@gmail.com>
2026-07-27 11:51:57 -0300

    POST

--- NodeExpress/teste.js
+++ NodeExpress/teste.js
@@ -9,6 +9,12 @@ app.get("/", (req, res) => {
   res.json({ total: mensagem.length, dados: mensagem });
 });
 
+app.post("/", (req, res) => {
+  const novaMsg = req.body;
+  mensagem.push(novaMsg);
+  res.status(201).json({ status: "Sucesso", dados: mensagem });
+});
+
 app.listen(3000, () => {
   console.log("Servidor ativo!");
 });
