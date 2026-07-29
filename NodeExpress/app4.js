bbcd82a000cf8cd2f3c2f8dcd36ee5116498a21f
Lucas <lucasrian.dev@gmail.com>
2026-07-29 15:31:18 -0300

    treinando patch e delete

--- NodeExpress/app4.js
+++ NodeExpress/app4.js
@@ -3,16 +3,21 @@
 const express = require("express");
 const app = express();
 
-app.use(express.text());
+app.use(express.json());
 
-let mensagem = [];
+const mensagem = [];
 
-app.patch("/", (req, res) => {
-  mensagem += req.body;
-  console.log(`Mensagem alterada ${mensagem}`);
-  res.send(`Mensagem nova alterada: ${mensagem}`);
+app.patch("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (!mensagem.id) {
+    return res.status(404).json({ status: "Sucesso", dados: mensagem });
+  }
+
+  mensagem = { ...mensagem[id], ...req.params };
+  res.status(201).json({ status: "Sucesso", dados: mensagem });
 });
 
 app.listen(3000, () => {
-  console.log("Server ligado!");
+  console.log("Server ativo!");
 });
--- NodeExpress/app5.js
+++ NodeExpress/app5.js
@@ -3,16 +3,22 @@
 const express = require("express");
 const app = express();
 
-let mensagem = "";
+app.use(express.json());
 
-app.use(express.text());
+const mensagem = [];
 
-app.delete("/", (req, res) => {
-  mensagem = "";
-  console.log(`Mensagem deletada! ${mensagem}`);
-  res.send(`Mensagem deletada!`);
+app.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (!mensagem.id) {
+    return res.status(201).json({ status: "Erro", dados: mensagem });
+  }
+
+  mensagem.splice(id, 1);
+
+  res.status(201).json({ status: "Deletado", dados: mensagem });
 });
 
 app.listen(3000, () => {
-  console.log("Servidor ligado!");
+  console.log("Server ativo!");
 });
