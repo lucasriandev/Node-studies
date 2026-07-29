2d66210f02dbace3aaa56004adcb8b5726bb6245
Lucas <lucasrian.dev@gmail.com>
2026-07-29 15:22:59 -0300

    Treinando put

--- NodeExpress/app3.js
+++ NodeExpress/app3.js
@@ -3,16 +3,22 @@
 const express = require("express");
 const app = express();
 
-app.use(express.text());
+app.use(express.json());
 
-let mensagem = "";
+const mensagem = [];
 
-app.put("/", (req, res) => {
-  mensagem = req.body;
-  console.log(`Mensagem substituida ${mensagem}`);
-  res.send(`Mensagem nova ${mensagem}`);
+app.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (!mensagem[id]) {
+    return res.status(404).json({ status: "Mensagem nao gravada!" });
+  }
+
+  mensagem[id] = req.body;
+
+  res.status(201).json({ status: "Sucesso", dados: mensagem });
 });
 
 app.listen(3000, () => {
-  console.log("Server ativado!");
+  console.log("Servidor ativo!");
 });
