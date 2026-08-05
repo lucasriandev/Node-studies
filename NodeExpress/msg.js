32a04975e14063f1172de86910c55a185e70e461
Lucas <lucasrian.dev@gmail.com>
2026-08-05 11:58:06 -0300

    Consertando prisma

--- NodeExpress/msg.js
+++ NodeExpress/msg.js
@@ -4,27 +4,28 @@ const { PrismaClient } = require("@prisma/client");
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   const todasMsg = await prisma.mensagem.findMany();
-  res.status(201).json({total: todasMsg.length, dados: todasMsg})
+  res.status(201).json({ total: todasMsg.length, dados: todasMsg });
 });
 
-router.post("/", (req, res) => {
-  
-  const {texto} = req.body
+router.post("/", async (req, res) => {
+  const { texto } = req.body;
 
   const novaMsg = await prisma.mensagem.create({
     data: {
-      texto: texto
-    }
-  })
-  res.status(201).json({status: "Sucesso", mensagemCriada: novaMsg})
+      texto: texto,
+    },
+  });
+  res.status(201).json({ status: "Sucesso", mensagemCriada: novaMsg });
 });
 
-
 router.delete("/", async (req, res) => {
-  await prisma.mensagem.deleteMany()
-  res.json({status: "Sucesso", detalhe: "Todas as mensagens apagadas do banco"})
+  await prisma.mensagem.deleteMany();
+  res.json({
+    status: "Sucesso",
+    detalhe: "Todas as mensagens apagadas do banco",
+  });
 });
 
 module.exports = router;
