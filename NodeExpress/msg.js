cae0c87000820e66b1d0cdfec51795bfe4f324e7
Lucas <lucasrian.dev@gmail.com>
2026-08-05 11:40:16 -0300

    mudando rotas

--- NodeExpress/msg.js
+++ NodeExpress/msg.js
@@ -1,52 +1,30 @@
 const express = require("express");
-const router = express.Router();
+const { PrismaClient } = require("@prisma/client");
 
-let mensagem = [];
+const router = express.Router();
+const prisma = new PrismaClient();
 
 router.get("/", (req, res) => {
-  res.json({ total: mensagem.length, dados: mensagem });
+  const todasMsg = await prisma.mensagem.findMany();
+  res.status(201).json({total: todasMsg.length, dados: todasMsg})
 });
 
 router.post("/", (req, res) => {
-  const novaMsg = req.body;
-  mensagem.push(novaMsg);
-  res.status(201).json({ status: "Sucesso", dados: mensagem });
+  
+  const {texto} = req.body
+
+  const novaMsg = await prisma.mensagem.create({
+    data: {
+      texto: texto
+    }
+  })
+  res.status(201).json({status: "Sucesso", mensagemCriada: novaMsg})
 });
 
-router.put("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
 
-  if (!mensagem[id]) {
-    return res.status(404).json({ erro: "Mensagem nao encontrada!" });
-  }
-
-  mensagem[id] = req.body;
-  //"Abra a gaveta de número id e coloque lá dentro o novo objeto que veio no req.body (substituindo o que estava lá antes)."
-  res.json({ status: "Sucesso", dados: mensagem[id] });
-});
-
-router.patch("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  if (!mensagem[id]) {
-    return res.status(404).json({ error: "Mensagem não encontrada!" });
-  }
-
-  mensagem[id] = { ...mensagem[id], ...req.body };
-
-  res.json({ status: "Sucesso", dados: mensagem[id] });
-});
-
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  if (!mensagem[id]) {
-    return res.status(404).json({ error: "Nada encontrado" });
-  }
-
-  mensagem.splice(id, 1);
-
-  res.status(200).json({ status: "Sucesso", dados: mensagem });
+router.delete("/", async (req, res) => {
+  await prisma.mensagem.deleteMany()
+  res.json({status: "Sucesso", detalhe: "Todas as mensagens apagadas do banco"})
 });
 
 module.exports = router;
