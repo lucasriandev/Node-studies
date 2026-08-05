498a3122d9bbf051616e35a4bb989c9caa92e85d
Lucas <lucasrian.dev@gmail.com>
2026-08-05 12:30:46 -0300

    prisma funcionando

--- NodeExpress/msg.js
+++ NodeExpress/msg.js
@@ -1,8 +1,11 @@
+require("dotenv/config");
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
-
+const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
 const router = express.Router();
-const prisma = new PrismaClient();
+
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 router.get("/", async (req, res) => {
   const todasMsg = await prisma.mensagem.findMany();
