72ea5b2a07a8a1572a19b683c54209acf0318096
Lucas <lucasrian.dev@gmail.com>
2026-08-07 13:03:33 -0300

    Projeto funcionando

--- DominandoBack/Pilar3/app.js
+++ DominandoBack/Pilar3/app.js
@@ -1,1 +1,16 @@
-import router from "./personagens";
+const express = require("express");
+const app = express();
+
+const rotasPersonagens = require("./personagens");
+
+app.use(express.json());
+
+app.use("/personagem", rotasPersonagens);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Rota nao encontrada!" });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor ativo!");
+});
--- DominandoBack/Pilar3/personagens.js
+++ DominandoBack/Pilar3/personagens.js
@@ -1,4 +1,41 @@
+require("dotenv/config");
 const express = require("express");
+const { PrismaClient } = require("./generated/prisma/client");
+const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
 const router = express.Router();
+
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+router.get("/", async (req, res) => {
+  const todosPersonagens = await prisma.personagem.findMany();
+  res
+    .status(201)
+    .json({ detalhe: todosPersonagens.length, dados: todosPersonagens });
+});
+
+router.post("/", async (req, res) => {
+  const { texto } = req.body;
+  const novaPersonagem = await prisma.personagem.create({
+    data: {
+      texto: texto,
+    },
+  });
+  res.status(201).json({ datalhe: "Sucesso no post", dados: novaPersonagem });
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    await prisma.personagem.delete({
+      where: {
+        id: id,
+      },
+    });
+  } catch (error) {
+    res.status(201).json({ error: "Erro ao deletar" });
+  }
+});
 
 module.exports = router;
