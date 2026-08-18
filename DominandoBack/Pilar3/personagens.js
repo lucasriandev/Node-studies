8730ca5e050b0ef1da4fbaf9e64d9a908d8bd6ec
Lucas <lucasrian.dev@gmail.com>
2026-08-18 12:42:20 -0300

    Install zod e cors no outro projeto

--- DominandoBack/Pilar3/personagens.js
+++ DominandoBack/Pilar3/personagens.js
@@ -2,40 +2,15 @@ require("dotenv/config");
 const express = require("express");
 const { PrismaClient } = require("./generated/prisma/client");
 const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
+const { z } = require("zod");
 const router = express.Router();
 
 const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
-router.get("/", async (req, res) => {
-  const todosPersonagens = await prisma.personagem.findMany();
-  res
-    .status(201)
-    .json({ detalhe: todosPersonagens.length, dados: todosPersonagens });
-});
-
-router.post("/", async (req, res) => {
-  const { texto } = req.body;
-  const novaPersonagem = await prisma.personagem.create({
-    data: {
-      texto: texto,
-    },
-  });
-  res.status(201).json({ datalhe: "Sucesso no post", dados: novaPersonagem });
-});
-
-router.delete("/:id", async (req, res) => {
-  const id = Number(req.params.id);
-
-  try {
-    await prisma.personagem.delete({
-      where: {
-        id: id,
-      },
-    });
-  } catch (error) {
-    res.status(201).json({ error: "Erro ao deletar" });
-  }
+const mensagemSchema = z.object({
+  nome: z.string().min(3, "Nome precisa ter no minimo tres letras"),
+  poder: z.string().min(2, "Jutsu errado!"),
 });
 
 module.exports = router;
