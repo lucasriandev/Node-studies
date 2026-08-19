7d7b574e8fc0898d84f8dd30bb8821463df22b57
Lucas <lucasrian.dev@gmail.com>
2026-08-19 15:31:38 -0300

    Rotas put, patch e del add

--- DominandoBack/Pilar3/personagens.js
+++ DominandoBack/Pilar3/personagens.js
@@ -39,4 +39,53 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const { nome, poder } = req.body;
+
+  try {
+    const novaMsg = await prisma.personagem.update({
+      where: {
+        id: id,
+      },
+      data: {
+        nome: nome,
+        poder: poder,
+      },
+    });
+    res.status(200).json({ status: "Atualizado com sucesso", dados: novaMsg });
+  } catch (error) {
+    res.status(201).json({ error: "Erro no put", error });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  const { texto } = req.body;
+
+  try {
+    const novoTexto = await prisma.personagem.update({
+      data: {
+        texto: texto,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({ error: "Erro no patch", error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    await prisma.personagem.delete({
+      where: { id: id },
+    });
+    res
+      .status(404)
+      .json({ status: "Sucesso", detalhe: `Mensagem ${idMsg} apagada` });
+  } catch (error) {
+    res.status(404).json({ error: "Erro ao deletar", error });
+  }
+});
+
 module.exports = router;
