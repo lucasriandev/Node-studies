5eccf65b64649925da315058433b4e33039f886f
Lucas <lucasrian.dev@gmail.com>
2026-08-22 14:56:37 -0300

    removendo por problema de prisma

--- DominandoBack/Pilar5/routes/animesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const mensagemController = require("../controllers/animesController");
-
-router.post("/", mensagemController.post);
-router.get("/", mensagemController.get);
-
-module.exports = router;
