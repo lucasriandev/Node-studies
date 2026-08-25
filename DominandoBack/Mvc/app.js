5c3a3e076290745a333ff40911e2308c7f36bcac
Lucas <lucasrian.dev@gmail.com>
2026-08-25 17:05:22 -0300

    att db

--- DominandoBack/Mvc/app.js
+++ DominandoBack/Mvc/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const rotasAnimes = require("./routes/AnimesRoutes");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/animes", rotasAnimes);
