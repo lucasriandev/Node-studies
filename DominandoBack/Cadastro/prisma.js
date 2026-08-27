b7f8bbb3f855ff87e3d798fd48f4325feda44905
Lucas <lucasrian.dev@gmail.com>
2026-08-27 16:18:51 -0300

    consertando erros do prisma

--- DominandoBack/Cadastro/prisma.js
+++ DominandoBack/Cadastro/prisma.js
@@ -1,9 +1,10 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaBetterSqlite } = require("@prisma/adapter-better-sqlite3");
+const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
 const Database = require("better-sqlite3");
+const path = require("path");
 
-const dbPath = process.env.DATABASE_URL.replace("file:", "");
-const adapter = new PrismaBetterSqlite(new Database(dbPath));
+const dbPath = path.join(__dirname, "prisma", "dev.db");
+const adapter = new PrismaBetterSqlite3(new Database(dbPath));
 const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
