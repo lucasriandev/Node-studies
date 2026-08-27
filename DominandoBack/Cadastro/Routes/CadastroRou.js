c863d3e79b203dbdbb96955df116de68cf0b590e
Lucas <lucasrian.dev@gmail.com>
2026-08-27 16:34:51 -0300

    Apagado por varios motivos de erros de versoes do prisma

--- DominandoBack/Cadastro/Routes/CadastroRou.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cadastroControllers = require("../Controllers/CadastroCont");
-
-router.post("/", cadastroControllers.post);
-router.get("/", cadastroControllers.get);
-
-module.exports = router;
