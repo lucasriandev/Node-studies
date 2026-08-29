5dcc0a90fc5c30f552285afb70325e5f71abfc77
Lucas <lucasrian.dev@gmail.com>
2026-08-29 12:09:16 -0300

    Post e get

--- DominandoBack/Cadastro/Controllers/cadastroCon.js
+++ DominandoBack/Cadastro/Controllers/cadastroCon.js
@@ -1,16 +1,34 @@
-const { z } = require("zod")
-const prisma = require("../prisma.js")
+const { z } = require("zod");
+const prisma = require("../prisma.js");
 
 const mensagemSchema = z.object({
-    email: z.string().min(3, "Email errado!"),
-    senha: z.number().int().positive("Senha precisa ser numero positivo!"),
+  email: z.string().min(3, "Email errado!"),
+  senha: z.number().int().positive("Senha precisa ser numero positivo!"),
 });
 
 const get = async (req, res) => {
-    const lista = await prisma.cadastros.findMany()
-    res.status(200).json({ detalhe: lista.length, dados: lista })
-}
+  const lista = await prisma.cadastros.findMany();
+  res.status(200).json({ detalhe: lista.length, dados: lista });
+};
+
+const post = async (req, res) => {
+  try {
+    const mensagemValidade = mensagemSchema.parse(req.body);
+
+    const novoCadastro = await prisma.cadastros.create({
+      data: {
+        email: mensagemValidade.email,
+        senha: mensagemValidade.senha,
+      },
+    });
+
+    res.status(201).json({ novoCadastro });
+  } catch (error) {
+    res.status(404).json({ status: "erro no post", error });
+  }
+};
 
 module.exports = {
-    get
-}   
+  get,
+  post,
+};
--- DominandoBack/Cadastro/Router/cadastroRou.js
+++ DominandoBack/Cadastro/Router/cadastroRou.js
@@ -1,5 +1,8 @@
-const express = require("express")
-const router = express.Router()
-const mensagemControllers = require("../Controllers/cadastroCon")
+const express = require("express");
+const router = express.Router();
+const mensagemControllers = require("../Controllers/cadastroCon");
 
-module.exports = router
+router.post("/", mensagemControllers.post);
+router.get("/", mensagemControllers.get);
+
+module.exports = router;
